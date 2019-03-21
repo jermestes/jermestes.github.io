69343c7a855fa457593b313df95b90eec2850b88
jermestes <estesjeremy96@gmail.com>
2019-03-21 18:11:50 -0400

    v0.2
    now w/ working mobile nav

---
+++ main.js
@@ -0,0 +1,19 @@
+//toggle function for nav bar
+const hamburger = document.getElementById('hamburger_menu');
+const navLinks = document.getElementById('mobile_nav_links');
+
+function toggle(){
+    if(navLinks.className == 'toggleOn'){
+        navLinks.classList.remove('toggleOn');
+        navLinks.classList.add('toggleOff');
+        hamburger.classList.remove('toggleOn');
+        hamburger.classList.add('toggleOff');
+    } else {
+        navLinks.classList.add('toggleOn');
+        navLinks.classList.remove('toggleOff');
+        hamburger.classList.add('toggleOn');
+        hamburger.classList.remove('toggleOff');
+    }
+}
+
+hamburger.addEventListener('click',toggle,false);
