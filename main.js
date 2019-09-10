82699d44f7cce75f75427d9e25ee200d23ce181e
jermestes <estesjeremy96@gmail.com>
2019-09-10 15:03:38 -0400

    tiny link change
    Changed empty resume html link to pdf link

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//Smooth scroll function
-//const navLinks = document.querySelectorAll("li a")
-//console.log(navLinks);
+const navLinks = document.querySelectorAll("li a")
+console.log(navLinks);
 
 const chevronBtn = document.querySelector("#header-art_chevron");
 console.log(chevronBtn);
 chevronBtn.addEventListener("click",smoothScroll);
 
+//Smooth scroll function
 function smoothScroll(event) {
     window.scroll({
         top: document.querySelector("#projects_section").offsetTop,
