834807216dc41d5835f8650e217cc01679ec629f
jermestes <estesjeremy96@gmail.com>
2019-10-09 16:59:10 -0400

    Nav & hamburger styling complete (minus slideout)

---
+++ src/components/HamburgerBTN.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const screenBreakpoint1 = '900px';
+
+//Hamburger button for mobile/small screen navigation
+const Hamburger = styled.button`
+    align-self: center;
+    border: none;
+    grid-column: 3/4;
+    grid-row: 1/3;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: end;
+    @media screen and (min-width: ${screenBreakpoint1}) {
+        display: none;
+    }
+    div {
+        height: 5.65px;
+        background-color: grey;
+        margin: .25rem 0;
+    }
+    div:first-child {
+        width: 47px;
+    }
+    div:nth-child(2) {
+        width: 36px;
+    }
+    div:last-child {
+        width: 25px;
+    }
+`;
+
+class HamburgerBTN extends Component {
+    render() {
+        return (
+            <Hamburger>
+                <div></div>  
+                <div></div>  
+                <div></div>                
+            </Hamburger>
+        );
+    }
+}
+
+export default HamburgerBTN;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
 import Logo from '../assets/logo.png';
+import HamburgerBTN from './HamburgerBTN';
+import Landing from './Landing';
 import About from './About';
-import Contact from './Contact';
 import Work from './Work';
+import Contact from './Contact';
 import Pdf from '../assets/jeremyestes_resume.pdf';
 import styled from 'styled-components';
-import Header_art from './Header_art';
 
 const screenBreakpoint1 = '900px';
 
+//The full nav, containing a <ul> list of <li> links
 const Navbar = styled.nav`
     text-transform: uppercase;
     display: grid;
     grid-template-rows: 3.75rem 3.75rem;  
-    grid-template-columns: repeat(12,1fr);
+    grid-template-columns: 1fr 1fr 1fr;
     background-color: #EEEEEE;
+    padding: 0 1rem;
+    @media screen and (min-width: ${screenBreakpoint1}) {
+        grid-template-columns: repeat(12,1fr);
+        padding: none;
+    }
     ul {
+        display: none;
         text-transform: uppercase;
         list-style: none;
         li a, li a:visited {
@@ -40,11 +48,14 @@ const Navbar = styled.nav`
     }
 `;
 
+//the top & bottom halves of the nav background
+//used to allow logo to have a color-split effect
 const Tophalf = styled.div`
     grid-column: 1/13;
     grid-row: 1/2;
     z-index: 10;
 `;
+
 
 const Bottomhalf = styled.div`
     grid-column: 1/13;
@@ -55,6 +66,7 @@ const Bottomhalf = styled.div`
     }
 `;
 
+//The nav logo/home button, an <img> contained in an <a>
 const Navlogo = styled(NavLink)`
     justify-self: center;
     align-self: center;
@@ -68,13 +80,10 @@ const Navlogo = styled(NavLink)`
             opacity: 0.9;
         }
     }
-    @media screen and (min-width: $screen-breakpoint1) {
+    @media screen and (min-width: ${screenBreakpoint1}) {
         grid-column: 1/4;
         grid-row: 1/3;
     }
-`;
-
-const Hamburger = styled.button`
 `;
 
 class Nav extends Component {
@@ -83,6 +92,7 @@ class Nav extends Component {
             <header>
                 <Navbar>
                     <Navlogo to="/"><img src={Logo} alt="Site Logo"/></Navlogo>
+                    <HamburgerBTN />
                     <ul>
                         <li><NavLink to="/about">About</NavLink></li>
                         <li><NavLink to="/work">Work</NavLink></li>
@@ -97,7 +107,7 @@ class Nav extends Component {
                     <Route path="/about" component={About} />
                     <Route path="/work" component={Work} /> 
                     <Route path="/contact" component={Contact} />   
-                    <Route exact path="/" component={Header_art} />
+                    <Route exact path="/" component={Landing} />
                 </Switch>            
             </header>
 
