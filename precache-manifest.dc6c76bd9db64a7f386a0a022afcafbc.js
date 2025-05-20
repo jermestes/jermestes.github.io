d31ece969191f943a27669d72276487dcd74afd0
Jeremy Estes <jerm@Jeremys-MacBook-Air.local>
2025-05-19 20:56:02 -0400

    Updates

---
+++ precache-manifest.dc6c76bd9db64a7f386a0a022afcafbc.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2cef3fe4bd678d1b9baa71422f98a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a329828bcacf06bfb84e",
+    "url": "/static/css/main.089ffaf8.chunk.css"
+  },
+  {
+    "revision": "baa0be8f554b95869c76",
+    "url": "/static/js/2.c3bc6eaf.chunk.js"
+  },
+  {
+    "revision": "a329828bcacf06bfb84e",
+    "url": "/static/js/main.02ac035b.chunk.js"
+  },
+  {
+    "revision": "3e1adb91db6e554843a8",
+    "url": "/static/js/runtime-main.513a0e3a.js"
+  },
+  {
+    "revision": "880f5c5bc098ba523b787d9a54a6f615",
+    "url": "/static/media/BMoviePoster-cropped.880f5c5b.jpg"
+  },
+  {
+    "revision": "70a6d9614eb185a516f986c8f419081d",
+    "url": "/static/media/ChakraPetch-Bold.70a6d961.ttf"
+  },
+  {
+    "revision": "d22f0ed901c2c4c5b92e021efd59cc68",
+    "url": "/static/media/ChakraPetch-Regular.d22f0ed9.ttf"
+  },
+  {
+    "revision": "8d6f6ff0bcc4512f99f05758b51590ec",
+    "url": "/static/media/GitHub-Mark.8d6f6ff0.eps"
+  },
+  {
+    "revision": "d26611a8033adc15cdd0bb7837e6d5bb",
+    "url": "/static/media/GitHub-Mark.d26611a8.ai"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "5d292e21532ea5857531cdb8b0180637",
+    "url": "/static/media/Octocat.5d292e21.jpg"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "3fc5b5134e2be742c3b6cc9fa4ff8d45",
+    "url": "/static/media/codepen-logo.3fc5b513.png"
+  },
+  {
+    "revision": "4a35f997f55da8ca2b123001fecb7f12",
+    "url": "/static/media/dribbble-ball-icon.4a35f997.eps"
+  },
+  {
+    "revision": "045744f7380e566afec93b5b31e2b4d3",
+    "url": "/static/media/dribbble-ball-mark.045744f7.svg"
+  },
+  {
+    "revision": "a2fead037b61d6cbcbbccd42e9577b1a",
+    "url": "/static/media/dribbble-ball-mark.a2fead03.png"
+  },
+  {
+    "revision": "e15bb79e1a9a3f03380edad387581d7a",
+    "url": "/static/media/dribbble-ball-mark.e15bb79e.eps"
+  },
+  {
+    "revision": "eb4ae6477f49590172e611dfe526ca05",
+    "url": "/static/media/dribbble-ball-mark.eb4ae647.psd"
+  },
+  {
+    "revision": "0762b915118d4340803a1dc805df4c56",
+    "url": "/static/media/dribbble-logo.0762b915.psd"
+  },
+  {
+    "revision": "220a2dfe0ac95a6a7c2a0180ed018269",
+    "url": "/static/media/dribbble-logo.220a2dfe.svg"
+  },
+  {
+    "revision": "500dbdff351d449a0bea1e6c674b3571",
+    "url": "/static/media/dribbble-logo.500dbdff.png"
+  },
+  {
+    "revision": "51b1a3be3dea29155b5641215a6780c0",
+    "url": "/static/media/dribbble-logo.51b1a3be.eps"
+  },
+  {
+    "revision": "6028f3e3f2ba12d7683f29003e97e441",
+    "url": "/static/media/dribbble-logo.6028f3e3.png"
+  },
+  {
+    "revision": "ce1a1cdb5459063c7d0e6891ad9530a8",
+    "url": "/static/media/dribbble-logo.ce1a1cdb.svg"
+  },
+  {
+    "revision": "ff43ebd155888cd3a9d6b02e1074ffa9",
+    "url": "/static/media/durant.ff43ebd1.jpg"
+  },
+  {
+    "revision": "815520d019b45b8fae22885e035eef39",
+    "url": "/static/media/gmail-logo.815520d0.webp"
+  },
+  {
+    "revision": "a390ae1157446fd2e76d35fd2712a133",
+    "url": "/static/media/gmail-logo.a390ae11.png"
+  },
+  {
+    "revision": "eefa9219d63fc2a390b3435564d1a181",
+    "url": "/static/media/grad-jerm2.eefa9219.jpg"
+  },
+  {
+    "revision": "b2d07afc4f36257a301f98195c587ddb",
+    "url": "/static/media/hero-wide.b2d07afc.jpg"
+  },
+  {
+    "revision": "a0788ea8568bdca5a72f1967df5dcec5",
+    "url": "/static/media/hero.a0788ea8.jpg"
+  },
+  {
+    "revision": "dbeecdc26e9bb06c317ba4c7e98877ab",
+    "url": "/static/media/idm211.dbeecdc2.PNG"
+  },
+  {
+    "revision": "11e331ff306f5c5726d53a4f37a8b10b",
+    "url": "/static/media/idm364.11e331ff.svg"
+  },
+  {
+    "revision": "b11cc6e53bacd453a8162f3b9ed926a6",
+    "url": "/static/media/jeremyestes-resume.b11cc6e5.pdf"
+  },
+  {
+    "revision": "b16e09340f1639f536ed38053a6ca96b",
+    "url": "/static/media/logo.b16e0934.png"
+  },
+  {
+    "revision": "f27ab29d7261538e4eefc6ddb27515d0",
+    "url": "/static/media/vinyl.f27ab29d.png"
+  },
+  {
+    "revision": "95be12873f6c09effc2acb67c45c8a02",
+    "url": "/static/media/work2.95be1287.jpg"
+  }
+]);
